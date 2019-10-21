92006e601ca2ae24409f42d9f81a274399f009ab
Panos Anastasiadis <anastapd@gmail.com>
2019-10-21 16:15:06 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <NavBar />
         <h1>Hello from React</h1>
       </div>
     );
