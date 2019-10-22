b187d1cd48bdeb880ab3b9df327d944e3f9ff254
Panos Anastasiadis <anastapd@gmail.com>
2019-10-22 13:39:12 +0100

    Implemented showing spinner while loading and renamed Users component with .js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-const Users = ({ users }) => {
+const Users = ({ users, loading }) => {
+  if (loading) return <Spinner />;
+
   return (
     <div style={userStyle}>
       {users.map(user => (
@@ -9,6 +13,11 @@ const Users = ({ users }) => {
       ))}
     </div>
   );
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 const userStyle = {
