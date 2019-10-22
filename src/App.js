41d0a794bd7478e8223f784da7e0d651175dcff6
Panos Anastasiadis <anastasiadis1@windowslive.com>
2019-10-23 00:18:01 +0100

    Added 5 most recent repos in user details page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import NavBar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Axios from 'axios';
-import Search from './components/users/Search';
-import Alert from './components/layout/Alert';
-import About from './components/pages/About';
-import User from './components/users/User';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Axios from "axios";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import User from "./components/users/User";
 
 class App extends Component {
   state = {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -39,6 +40,16 @@ class App extends Component {
     this.setState({ loading: false, user: res.data });
   };
 
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await Axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ loading: false, repos: res.data });
+  };
+
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => {
@@ -47,17 +58,17 @@ class App extends Component {
   };
 
   render() {
-    const { loading, users, alert, user } = this.state;
+    const { loading, users, alert, user, repos } = this.state;
 
     return (
       <Router>
-        <div className='App'>
+        <div className="App">
           <NavBar />
           <Alert alert={alert} />
           <Switch>
             <Route
               exact
-              path='/'
+              path="/"
               render={props => (
                 <Fragment>
                   <Search
@@ -66,21 +77,23 @@ class App extends Component {
                     searchUsers={this.searchUsers}
                     setAlert={this.setAlert}
                   />
-                  <div className='container'>
+                  <div className="container">
                     <Users loading={loading} users={users} />
                   </div>
                 </Fragment>
               )}
             />
-            <Route exact path='/about' component={About} />
+            <Route exact path="/about" component={About} />
             <Route
               exact
-              path='/user/:login'
+              path="/user/:login"
               render={props => (
                 <User
                   {...props}
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
                   user={user}
+                  repos={repos}
                   loading={loading}
                 />
               )}
