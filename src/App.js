b580ebe41f312e0bdbb546581961c72733d04b15
Panos Anastasiadis <anastapd@gmail.com>
2019-10-22 15:11:25 +0100

    Implemented searching users via search text and added alert message if search text is empty

--- src/App.js
+++ src/App.js
@@ -3,27 +3,50 @@ import './App.css';
 import NavBar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Axios from 'axios';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
-  async componentDidMount() {
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  searchUsers = async text => {
     this.setState({ loading: true });
+
     const res = await Axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ loading: false, users: res.data });
-  }
+    this.setState({ loading: false, users: res.data.items });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
+    const { loading, users, alert } = this.state;
+
     return (
       <div className='App'>
         <NavBar />
+        <Alert alert={alert} />
+        <Search
+          showClear={users.length > 0}
+          clearUsers={this.clearUsers}
+          searchUsers={this.searchUsers}
+          setAlert={this.setAlert}
+        />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
