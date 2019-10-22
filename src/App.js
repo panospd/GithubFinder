247f5ae1285e67af28d95bbacdb29b488605a56a
Panos Anastasiadis <anastapd@gmail.com>
2019-10-22 11:57:01 +0100

    Added static data for user items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
-import NavBar from "./components/layout/Navbar";
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <NavBar />
-        <h1>Hello from Mint 19 linux distro</h1>
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
